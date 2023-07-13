d8a9c504521a2847839dc91b987cb587f80465e9
Mike Fredette <mikefredette@mac.com>
2023-07-13 10:25:05 -0700

    Create Change Computer Name.sh

---
+++ Change Computer Name.sh
@@ -0,0 +1,121 @@
+﻿#!/bin/bash
+
+####################################################################################################
+#
+#   swiftDialog Choose Jamf Site
+#   https://github.com/beatlemike/swiftDialog-Scripts
+#
+#   Purpose: Change Computer Name
+#
+####################################################################################################
+#
+# HISTORY
+#
+# Version 0.0.1, 06-July-2023, Mike Fredette (@beatlemike)
+# - Original version
+#
+####################################################################################################
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Turn off Jamf binary check-in (This can be commented out if not used for setup.)
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+launchctl unload /Library/LaunchDaemons/com.jamfsoftware.task.1.plist
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Pre-flight Check: Validate / install swiftDialog (Borrowed from @dan-snelson and @acodega!)
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+function dialogCheck() {
+
+    # Get the URL of the latest PKG From the Dialog GitHub repo
+    dialogURL=$(curl --silent --fail "https://api.github.com/repos/bartreardon/swiftDialog/releases/latest" | awk -F '"' "/browser_download_url/ && /pkg\"/ { print \$4; exit }")
+
+    # Expected Team ID of the downloaded PKG
+    expectedDialogTeamID="PWA5E9TQ59"
+
+    # Check for Dialog and install if not found
+    if [ ! -e "/Library/Application Support/Dialog/Dialog.app" ]; then
+
+        # Create temporary working directory
+        workDirectory=$( /usr/bin/basename "$0" )
+        tempDirectory=$( /usr/bin/mktemp -d "/private/tmp/$workDirectory.XXXXXX" )
+
+        # Download the installer package
+        /usr/bin/curl --location --silent "$dialogURL" -o "$tempDirectory/Dialog.pkg"
+
+        # Verify the download
+        teamID=$(/usr/sbin/spctl -a -vv -t install "$tempDirectory/Dialog.pkg" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()')
+
+        # Install the package if Team ID validates
+        if [[ "$expectedDialogTeamID" == "$teamID" ]]; then
+
+            /usr/sbin/installer -pkg "$tempDirectory/Dialog.pkg" -target /
+            sleep 2
+            dialogVersion=$( /usr/local/bin/dialog --version )
+
+        else
+
+            # Display a so-called "simple" dialog if Team ID fails to validate
+            osascript -e 'display dialog "Please advise your Support Representative of the following error:\r\r• Dialog Team ID verification failed\r\r" with title "Setup Your Mac: Error" buttons {"Close"} with icon caution'
+            completionActionOption="Quit"
+            exitCode="1"
+            quitScript
+
+        fi
+
+        # Remove the temporary working directory when done
+        /bin/rm -Rf "$tempDirectory"
+
+    fi
+
+}
+
+if [[ ! -e "/Library/Application Support/Dialog/Dialog.app" ]]; then
+    dialogCheck
+fi
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Change Computer Name
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+dialogApp="/usr/local/bin/dialog"
+
+title="Name This Mac"
+message="Enter the preferred computer name below"
+
+hwType=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Model Identifier" | grep "Book")  
+if [ "$hwType" != "" ]; then
+  icon="SF=laptopcomputer"
+  else
+  icon="SF=desktopcomputer"
+fi
+
+dialogCMD="$dialogApp -p --title \"$title\" \
+--icon \"$icon\" \
+--message \"$message\" \
+--small \
+--textfield \"Computer Name\""
+
+computerName=$(eval "$dialogCMD" | awk -F " : " '{print $NF}')
+
+scutil --set HostName "$computerName"
+scutil --set LocalHostName "$computerName"
+scutil --set ComputerName "$computerName"
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Turn on Jamf binary check-in
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+launchctl load /Library/LaunchDaemons/com.jamfsoftware.task.1.plist
